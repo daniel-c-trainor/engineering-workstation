da325963139d9ac26cb2bc2cd4654c976ff1071b
daniel-c-trainor <dnnytrainor@gmail.com>
2026-08-11 08:09:56 +0100

    added bad code to test sonar

---
+++ bad_code.py
@@ -0,0 +1,17 @@
+def add(a, b):
+    return a + b
+
+def unused_function():
+    x = 123
+    y = 456
+    return x * y
+
+print(add(1, 2))
+
+def bad():
+    x = 1
+    y = 2
+    return x / 0
+
+def bad():
+    return 1 / 0
